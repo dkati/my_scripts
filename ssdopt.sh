3fbf35c42e5e8a34549f332cf2741454d035dece
Dimitris Katikaridis <dkatikaridis@gmail.com>
2017-11-29 20:37:41 +0200

    Create ssdopt.sh

---
+++ ssdopt.sh
@@ -0,0 +1,12 @@
+echo "Disabling swappiness..."
+echo -e "vm.swappiness=0" | sudo tee -a /etc/sysctl.conf
+echo "Creating a cron executable on daily basis..."
+echo -e "#\x21/bin/sh\\nfstrim -v /" | sudo tee /etc/cron.daily/trim
+echo "Allowing system to execute the trim job..."
+sudo chmod +x /etc/cron.daily/trim
+
+echo "Manually change the mount flag by replacing"
+echo "'errors=remount-ro' to 'noatime,errors=remount-ro' "
+echo "Sleeping for 3 seconds and opening the file..."
+sleep 3
+sudo nano /etc/fstab
