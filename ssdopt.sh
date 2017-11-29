bb15ac4d0e9a191ba4c91398e5ddf2f2a0a76850
Dimitris Katikaridis <dkatikaridis@gmail.com>
2017-11-29 20:42:29 +0200

    ssdopt: Trim NOW

--- ssdopt.sh
+++ ssdopt.sh
@@ -4,6 +4,10 @@ echo "Creating a cron executable on daily basis..."
 echo -e "#\x21/bin/sh\\nfstrim -v /" | sudo tee /etc/cron.daily/trim
 echo "Allowing system to execute the trim job..."
 sudo chmod +x /etc/cron.daily/trim
+echo "Now trimming the SSD"
+echo ""
+sudo fstrim -v /
+echo ""
 
 echo "Manually change the mount flag by replacing"
 echo "'errors=remount-ro' to 'noatime,errors=remount-ro' "
